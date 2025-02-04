3d888f679862091ba3bb21f16cd80bfd8d466ffc
SZPY1950 <savioz.py@protonmail.com>
2025-02-04 23:15:55 +0100

    Update reset_db.sql
    corrected reset

--- BookSwipe_backend/reset_db.sql
+++ BookSwipe_backend/reset_db.sql
@@ -5,9 +5,16 @@ SELECT pg_terminate_backend(pg_stat_activity.pid)
 FROM pg_stat_activity
 WHERE datname = 'BinderDBTest' AND pid <> pg_backend_pid();
 
--- Drop the database
+-- Drop and recreate the database
 DROP DATABASE IF EXISTS "BinderDBTest";
 CREATE DATABASE "BinderDBTest";
 
-DROP SCHEMA public CASCADE;
+-- Connect to the new database and reset its schema
+\c BinderDBTest;
+
+DROP SCHEMA IF EXISTS public CASCADE;
 CREATE SCHEMA public;
+
+-- Grant privileges (optional but recommended)
+ALTER SCHEMA public OWNER TO postgres;
+
